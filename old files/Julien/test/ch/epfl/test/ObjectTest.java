00396af96e22add0b20b12323ceea0daf6538796
AntoineCrettenand <lugiar13@gmail.com>
2017-11-01 01:39:05 +0100

    Delete Files

--- old files/Julien/test/ch/epfl/test/ObjectTest.java
+++
@@ -1,7 +0,0 @@
-package ch.epfl.alpano.test;
-
-public interface ObjectTest {
-    static boolean hashCodeIsCompatibleWithEquals(Object o1, Object o2) {
-        return ! o1.equals(o2) || o1.hashCode() == o2.hashCode();
-    }
-}
